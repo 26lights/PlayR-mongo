e80fb8664302052fd6eae38629a21a16fc40314e
Olivier Samyn <olivier@26lights.com>
2014-04-25 14:39:57 +0200

    Add simple id based selector shortcut and Resource shortcut traits.

--- app/twentysix/playr/mongo/MongoResource.scala
+++ app/twentysix/playr/mongo/MongoResource.scala
@@ -1,6 +1,7 @@
 package twentysix.playr.mongo
 
 import reflect.runtime.universe.{Type,TypeTag,typeOf}
+import play.modules.reactivemongo.json.BSONFormats.BSONObjectIDFormat
 import scala.language.implicitConversions
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.modules.reactivemongo.json.collection.JSONCollection
@@ -16,6 +17,8 @@ abstract class MongoResource[R:Format] extends Resource[BSONObjectID, R] {
 
   def jsonGenerateId = __.json.update((__ \ '_id).json.put(BSONFormats.BSONObjectIDFormat.writes(BSONObjectID.generate)))
 
+  def idSelector(bid: BSONObjectID) = Json.obj("_id" -> bid)
+
   def collection: JSONCollection
 
   def parseId(sid: String) = BSONObjectID.parse(sid).toOption
@@ -27,3 +30,17 @@ object MongoResource {
   implicit def mongoIdResourceAction[R, C<:MongoResource[R]](f: (JsObject, R)=> EssentialAction)(implicit tt: TypeTag[(JsObject, R)=> EssentialAction]) =
     Resource.mongoResourceAction[BSONObjectID, R, C](f)
 }
+
+
+abstract class MongoReadController[R:Format] extends MongoResource[R]
+                                                with ResourceRead
+
+abstract class MongoRwController[R:Format] extends MongoReadController[R]
+                                              with ResourceRead
+                                              with ResourceUpdate
+                                              with ResourceCreate
+
+abstract class MongoCrudController[R:Format] extends MongoReadController[R]
+                                                with ResourceWrite
+                                                with ResourceDelete
+                                                with ResourceCreate
